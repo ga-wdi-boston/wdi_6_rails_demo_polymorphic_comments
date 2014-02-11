798d83b0957c4680c8c96768b1fb2fce7f1e5273
David Fisher <tibbon@gmail.com>
2014-02-11 12:47:55 -0500

    rails g scaffold Computer make:text model:text price:decimal

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 PolyDemo::Application.routes.draw do
+  resources :computers
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
