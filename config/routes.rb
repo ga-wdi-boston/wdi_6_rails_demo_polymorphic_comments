fa2174425844903c40eed996b4306f06afb6b633
David Fisher <tibbon@gmail.com>
2014-02-11 14:26:00 -0500

    Updated routes for proper defaults

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 PolyDemo::Application.routes.draw do
 
   resources :computers do
-    resources :comments, defaults: {commentable: 'photo'}
+    resources :comments, defaults: {commentable: 'computer'}
   end
 
   resources :cars do
-    resources :comments, defaults: {commentable: 'post'}
+    resources :comments, defaults: {commentable: 'car'}
   end
 
 end
