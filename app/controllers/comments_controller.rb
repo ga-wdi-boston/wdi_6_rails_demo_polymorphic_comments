676ddb723626ba186247140aebc27bf73dbaf631
David Fisher <tibbon@gmail.com>
2014-02-11 14:47:58 -0500

    Updated controller and index view for Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :set_commentable # Gives us @commentable on all actions
 
   # GET /comments
   # GET /comments.json
@@ -65,6 +66,14 @@ class CommentsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
       @comment = Comment.find(params[:id])
+    end
+
+    def set_commentable
+      @commentable = params[:commentable].classify.constantize.find(commentable_id)
+    end
+
+    def commentable_id
+      params[(params[:commentable] + "_id").to_sym]
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
